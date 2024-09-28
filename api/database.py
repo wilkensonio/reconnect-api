4583797c3132e6d4ab01861fbeb3abe3e7f4a206
Wilkenson <wilkensoncode@gmail.com>
2024-09-28 14:29:16 -0400

    Set CI/CD pipeline to docker using github actions

--- api/database.py
+++ api/database.py
@@ -4,11 +4,15 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+DB_PORT = os.getenv("DB_PORT")
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
-
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{
+    DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- tests/test_user_get_users.py
+++ tests/test_user_get_users.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 def test_user_signup(client):
     user_data = {
         "user_id": "70573536",
@@ -10,19 +13,25 @@ def test_user_signup(client):
 
     response = client.post("/api/v1/signup/", json=user_data)
     assert response.status_code == 200
-    assert response.json()["email"] == "john.doe@example.com"
+    assert response.json()[
+        "email"] == "john.doe@example.com", "Email returned does not match the expected email"
 
     # Test that the user is already registered
     response = client.post("/api/v1/signup/", json=user_data)
     assert response.status_code == 400
-    assert response.json()["detail"] == "Email already registered"
+    assert response.json()[
+        "detail"] == "Email already registered", "Expected error message for already registered email"
 
     # get the user by email
     response = client.get("/api/v1/user/email/john.doe@example.com")
     assert response.status_code == 200
-    assert response.json()["email"] == "john.doe@example.com"
+    assert response.json()[
+        "email"] == "john.doe@example.com", "Email returned does not match the expected email"
 
     # get all users
+
+    current_date = datetime.now().strftime("%B %d, %Y")
+
     response = client.get("/api/v1/users/")
     assert response.status_code == 200
     assert response.json() == [
@@ -33,11 +42,12 @@ def test_user_signup(client):
             "last_name": "Doe",
             "email": "john.doe@example.com",
             "phone_number": "2036908924",
-            "created_at": "September 27, 2024"
+            "created_at": current_date
         }
-    ]
+    ], "Fetched user list does not match the expected user list"
 
     # get the user by id
     response = client.get("/api/v1/user/id/70573536")
     assert response.status_code == 200
-    assert response.json()["user_id"] == "70573536"
+    assert response.json()[
+        "user_id"] == "70573536", "User ID returned does not match the expected user ID"
